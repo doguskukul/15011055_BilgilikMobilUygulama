409e63f6d9dc3a17c45469c931d25b78f1d15e20
Ilkem Inan Ak <ilkeminan1@gmail.com>
2019-03-24 21:21:42 +0300

    android studio

---
+++ app/src/main/java/com/example/deneme/MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.deneme;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+public class MainActivity extends AppCompatActivity {
+    EditText txtInput1,txtInput2,txtInput3,txtInput4,txtInput5,txtInput6;
+    int counter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        txtInput1 = (EditText) this.findViewById(R.id.ad);
+        txtInput2 = (EditText) this.findViewById(R.id.soyad);
+        txtInput3 = (EditText) this.findViewById(R.id.kimlikno);
+        txtInput4 = (EditText) this.findViewById(R.id.dogumtarihi);
+        txtInput5 = (EditText) this.findViewById(R.id.dogumyeri);
+        txtInput6 = (EditText) this.findViewById(R.id.telefonno);
+        if(savedInstanceState != null){
+            String message = savedInstanceState.getString("msj");
+            Toast.makeText(this,message,Toast.LENGTH_LONG).show();
+            counter=savedInstanceState.getInt("counter",0);
+        }
+        Button button1 = (Button) this.findViewById(R.id.button);
+        Button button3 = (Button) this.findViewById(R.id.button3);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this,sayfa2.class);
+                i.putExtra("mesaj",txtInput1.getText().toString());
+                i.putExtra("mesaj2",txtInput2.getText().toString());
+                i.putExtra("mesaj3",txtInput3.getText().toString());
+                i.putExtra("mesaj4",txtInput4.getText().toString());
+                i.putExtra("mesaj5",txtInput5.getText().toString());
+                i.putExtra("mesaj6",txtInput6.getText().toString());
+                startActivity(i);
+            }
+        });
+        button3.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v2) {
+                txtInput1.getText().clear();
+                txtInput2.getText().clear();
+                txtInput3.getText().clear();
+                txtInput4.getText().clear();
+                txtInput5.getText().clear();
+                txtInput6.getText().clear();
+            }
+        });
+    }
+    public void onResume(){
+        super.onResume();
+        counter++;
+    }
+    public void onSaveInstanceState(Bundle savedInstanceState){
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putString("message","This is a saved message");
+        savedInstanceState.putInt("counter",counter);
+    }
+    public void onRestoreInstanceState(Bundle savedInstanceState){
+        super.onRestoreInstanceState(savedInstanceState);
+        Toast.makeText(getApplicationContext(),"onRestoreInstanceState",Toast.LENGTH_SHORT).show();
+        counter= savedInstanceState.getInt("counter",0);
+    }
+}
---
+++ app/src/main/java/com/example/deneme/sayfa2.java
@@ -0,0 +1,52 @@
+package com.example.deneme;
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Button;
+import android.view.View;
+import android.content.Context;
+import android.widget.Toast;
+public class sayfa2 extends AppCompatActivity {
+    Context context=this;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sayfa2);
+        TextView textViewAd = (TextView) this.findViewById(R.id.ad2);
+        TextView textView2 = (TextView) this.findViewById(R.id.soyad2);
+        TextView textView3 = (TextView) this.findViewById(R.id.kimlikno2);
+        TextView textView4 = (TextView) this.findViewById(R.id.tarih2);
+        TextView textView5 = (TextView) this.findViewById(R.id.yer2);
+        final TextView textView6 = (TextView) this.findViewById(R.id.telefonno2);
+        Intent i = getIntent();
+        String s = i.getStringExtra("mesaj");
+        String s2 = i.getStringExtra("mesaj2");
+        String s3 = i.getStringExtra("mesaj3");
+        String s4 = i.getStringExtra("mesaj4");
+        String s5 = i.getStringExtra("mesaj5");
+        String s6 = i.getStringExtra("mesaj6");
+        textViewAd.setText(s);
+        textView2.setText(s2);
+        textView3.setText(s3);
+        textView4.setText(s4);
+        textView5.setText(s5);
+        textView6.setText(s6);
+        Button button2 = (Button) this.findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent i2 = new Intent(Intent.ACTION_CALL);
+                i2.setData(Uri.parse("tel:" + textView6.getText().toString()));
+                if(ContextCompat.checkSelfPermission(context, Manifest.permission.CALL_PHONE)!= PackageManager.PERMISSION_GRANTED){
+                    Toast.makeText(getApplicationContext(), "Ayarlar/Uygulamalar/Kimlik/İzinler/Telefon : izni aç", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                startActivity(i2);
+            }
+        });
+    }
+}
